a40a673e13959e209ddeca691ecb4da4481a33da
MijnendonckxYara <yara.mijnendonckx@student.uhasselt.be>
2022-10-20 13:49:40 +0200

    keywords in hashmap

--- Taak 1/Taak1/Taak1/Main.cpp
+++ Taak 1/Taak1/Taak1/Main.cpp
@@ -5,16 +5,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unordered_map>
 
 int main() {
 	std::string myText;
 
 	std::ifstream myReadFile("keywords.txt");
 
-	while (std::getline(myReadFile, myText))
-		std::cout << myText << std::endl;
+	std::unordered_map<std::string, int> hash;
+
+	while (std::getline(myReadFile, myText)) {
+		hash[myText] = 0;
+	}
+
+	// Key en values van hashmap printen
+	for (auto x : hash) {
+		std::cout << x.first << " " << x.second << std::endl;
+	}
+
 	myReadFile.close();
 
 	return 0;
 }
-
